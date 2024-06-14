0300d7079c01e95c539d6961f3d294da81e84a63
mayusB <54024278+mayusB@users.noreply.github.com>
2024-06-14 18:43:13 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,19 @@
 # ruleid: command-injection-os-system
 image = request.POST['image']
 os.system("./face-recognize %s --N 24" % image)
+
+import json
+from json import load, loads
+
+def lambda_handler(event, context):
+
+    tainted = event['exploit_code']
+    s = "encoded-data"
+    b = bytearray().extend(map(ord,string))
+
+    # ruleid: tainted-json-aws-lambda
+    json.load(tainted)
+    # # ruleid: tainted-json-aws-lambda
+    # json.load(tainted, cls=None, object_hook=None, parse_float=None, parse_int=None, parse_constant=None, object_pairs_hook=None)
+    # # ruleid: tainted-json-aws-lambda
+    # json.loads(tainted)
